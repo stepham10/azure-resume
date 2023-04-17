0142d93931a40d5e5aee7b4032deb2083e43612c
stepham10 <stepham10@gmail.com>
2023-04-16 19:59:16 -0500

    Deployed Storage and Function

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter'; 
+const functionApiUrl = 'https://stephenphamgetresumecounter.azurewebsites.net/api/GetResumeCounter?code=Iz_tw9h2Ghy2U9zcIXIGVrIfP-_CbGqEh-G2fxUpuphsAzFu0qIfgg=='
+const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter'; 
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.");
